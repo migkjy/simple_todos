a84e66e3741998c7fa66640b6608f074a2b1f3ae
June Kim <migkjy@naver.com>
2017-11-08 03:16:17 -0800

    step4 forms and events finished

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+// collections.js was stored at lib folder before, now it is at imports folder. need to import tasks.js
+import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
